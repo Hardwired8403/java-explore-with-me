f2bfd45b29e495b02f40b754f13de63996025090
Hardwired8403 <zerox@zerosystem.ru>
2024-02-24 15:49:45 +0300

    revert некторых изменений

--- ewm-main-service/src/main/java/ru/practicum/main/event/service/EventPrivateServiceImpl.java
+++ ewm-main-service/src/main/java/ru/practicum/main/event/service/EventPrivateServiceImpl.java
@@ -248,11 +248,11 @@ public class EventPrivateServiceImpl implements EventPrivateService {
                 return new NotFoundException("Request not found");
             });
 
-            if (Objects.equals(requestShort.getStatus(),Status.CONFIRMED)) {
+            if (requestShort.getStatus().equals(Status.CONFIRMED)) {
                 request.setStatus(Status.CONFIRMED);
                 updateRequest.getConformedRequest().add(request);
             }
-            if (Objects.equals(requestShort.getStatus(), Status.REJECTED)) {
+            if (requestShort.getStatus().equals(Status.REJECTED)) {
                 request.setStatus(Status.REJECTED);
                 updateRequest.getCanselRequest().add(request);
             }
